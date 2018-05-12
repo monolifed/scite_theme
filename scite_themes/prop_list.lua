cd3bcfc8e747db030e4044a1cc5c9676bf6f2234
monolifed <6624464+monolifed@users.noreply.github.com>
2018-05-12 18:41:47 +0300

    more automation

---
+++ scite_themes/prop_list.lua
@@ -0,0 +1,17 @@
+local prop_list = {
+	'editor.properties',
+	'theme.properties',
+	'lang_all.properties',
+	'lang_bash.properties',
+	'lang_batch.properties',
+	'lang_cpp.properties',
+	'lang_lua.properties',
+	'lang_makefile.properties',
+	'lang_props.properties',
+	'lang_ruby.properties',
+
+	
+	'lang_yaml.properties',
+	'errorlist.properties',
+}
+return prop_list
